82125d99021f40688c81b79cb24c31479dd65faa
romanzhukovv <zhukov.roman89@gmail.com>
2022-08-30 23:24:19 +0300

    Added extension folder

---
+++ Pulse/Extensions/UIView+Extension.swift
@@ -0,0 +1,37 @@
+//
+//  UIView+Extension.swift
+//  Pulse
+//
+//  Created by Roman Zhukov on 30.08.2022.
+//
+
+import UIKit
+
+extension UIView {
+    func makeSystem(_ button: UIButton) {
+        button.addTarget(self, action: #selector(handleIn), for: [
+            .touchDown,
+            .touchDragInside
+        ])
+        
+        button.addTarget(self, action: #selector(handleOut), for: [
+            .touchDragOutside,
+            .touchUpInside,
+            .touchUpOutside,
+            .touchDragExit,
+            .touchCancel
+        ])
+    }
+    
+    @objc func handleIn() {
+        UIView.animate(withDuration: 0.15) {
+            self.alpha = 0.55
+        }
+    }
+    
+    @objc func handleOut() {
+        UIView.animate(withDuration: 0.15) {
+            self.alpha = 1
+        }
+    }
+}
